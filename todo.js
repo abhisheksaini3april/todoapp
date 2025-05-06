d18efbe47cf7dd07f31120825d1c666f601a4c22
abhishek <abhishek.sainiapril@gmail.com>
2025-05-06 15:54:47 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,51 @@
+let input = document.querySelector(".in");
+let button = document.querySelector(".add");
+let taskmai = document.querySelector(".taskmains")
+
+function addTask()
+{
+    if(input.value == "")
+    {
+        alert("please enter the task");
+    }
+    else
+    {
+        let child = document.createElement("li");
+        child.textContent= input.value;
+        // let taskmai = document.querySelector(".taskmains");
+        taskmai.appendChild(child);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        child.appendChild(span);
+        input.value = "";
+    }
+
+    saveData();
+}
+
+taskmai.addEventListener("click",function(e)
+{
+    if(e.target.tagName ==="LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+
+   else if(e.target.tagName ==="SPAN")
+   {
+    e.target.parentElement.remove();
+    saveData();
+   }
+},false)
+
+
+function saveData()
+{
+    localStorage.setItem("data",taskmai.innerHTML);
+}
+
+function showTask()
+{
+    taskmai.innerHTML=localStorage.getItem("data");
+}
+showTask();
